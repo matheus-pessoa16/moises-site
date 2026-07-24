c2ca5793bf0de3979acb9075836b3202f7921fe3
Matheus Pessoa <matheus.pessoa16@gmail.com>
2026-07-24 16:11:33 -0300

    feat(data): create placeholder campaign cards data

---
+++ apps/web/src/data/campaigns.js
@@ -0,0 +1,24 @@
+export const campaigns = [
+  {
+    id: 'finecap-2024',
+    title: 'Finecap 2024',
+    description: 'Confira as estruturas e cenografias que montamos para o maior evento do ano.',
+    image: 'https://lh3.googleusercontent.com/d/1kYT20yjI-B6chpBdskr5ErMkRArqoyiZ=w1000?authuser=0',
+    ctaText: 'Ver projeto',
+    ctaLink: '/galeria?categoria=finecap',
+  },
+  {
+    id: 'natal-na-serra',
+    title: 'Natal na Serra',
+    description: 'Cenografia festiva de Natal em Portalegre/RN — um espetáculo visual imperdível.',
+    image: 'https://lh3.googleusercontent.com/d/1hknoh1GflWnqIw6lcrzwBiHjlrTAdDte=w1000?authuser=0',
+    ctaText: 'Ver projeto',
+    ctaLink: '/galeria?categoria=natal_na_serra',
+  },
+  {
+    id: 'trabalhe-conosco',
+    title: 'Trabalhe Conosco',
+    description: 'Faça parte da nossa equipe! Envie seu currículo e venha criar comunicação visual com a gente.',
+    image: 'https://lh3.googleusercontent.com/d/18xOXZzf3FW_ptUF9UNNSdTxL6ec5QnfY=w1000?authuser=0',
+  },
+]
